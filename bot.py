2dd82d0a0f2149f8bdfbe37a52549ca7a6837bd8
vladaad <59199127+vladaad@users.noreply.github.com>
2021-05-25 18:18:42 +0200

    i hate myself

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 # Imports
-import os
 import discord
+import random
 
 # Settings
 token = open("token.txt").readlines()[0].replace("\n", "")
@@ -8,11 +8,19 @@ guild = open("guild.txt").readlines()[0].replace("\n", "")
 
 # Idk copy pasted this
 
-class CustomClient(discord.Client):
-    async def on_ready(self):
-        print(f'{self.user} has connected to Discord!')
+client = discord.Client()
 
-client = CustomClient()
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
 
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    else:
+        response = ("x" + "D" * random.randint(20, 35))
+        await message.channel.send(response)
 
 client.run(token)
